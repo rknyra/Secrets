245a5dc0d162fa5f5f1e5ec97208001fc6320187
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-30 10:20:03 +0300

    add method to log in registered users

--- app/auth/views.py
+++ app/auth/views.py
@@ -21,5 +21,22 @@ def register():
         db.session.commit()
 
         return redirect(url_for('auth.login'))
-    title = "New Account"
+    title = "New Secrets Account"
     return render_template('auth/register.html',registration_form = form, title = title )
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    '''
+    Method to login registered users
+    '''
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Secrets login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
