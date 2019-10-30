465ef8dbb6a18be37c769d9b8a4165ed799655b3
RKNyra <rk.nyra@gmail.com>
2019-10-30 08:46:42 +0300

    get the necessary installations and write the necessary lines of code so that the app runs from python3.6 manage.py server.

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+
+class Config:
+    '''
+    General configurations (configs) parent class
+    '''
+    
+    pass 
+    
+
+class ProdConfig(Config):
+    '''
+    Production configs child class
+    
+    Args:
+        Config: The parent config class with General configs settings
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configs child class
+    
+    Args:
+        Config: The parent config class with General configs settings
+    '''
+    
+    DEBUG = True
+    
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
