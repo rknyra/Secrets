34ff765f608c230c1d0cd88c37b889801f7b90ca
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-30 16:13:11 +0300

    create new files and configurations

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/rknyra/Documents/Moringa School/Secrets/virtual"
+set -gx VIRTUAL_ENV "/home/moringa/Documents/secrets/Secrets/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
