88112835e82af7c5e55b4b3a23cd7e4d606ce789
Tyra-hans <tyrahans17@gmail.com>
2019-10-31 12:03:53 +0300

    Creates a git ignore file that will have pyc and virtual

--- app/main/views.py
+++ app/main/views.py
@@ -11,13 +11,6 @@ def post():
     return render_template('secret.html')
 
 @main.route("/post/new", methods=['GET','POST'])
-# @login_required
 def new_post():
     form= SecretForm()
-    
-    # if form.validate_on_submit():
-        # post = Post(title = form.title.data,content = form.content.data, author=current_user)
-        
-    # flash('Post created', 'success')
-    # return redirect(url_for('main.home'))
     return render_template('new_secret.html',title='New post', form=form)
