186cce02a653f20f24489af50b80195bf48de57e
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-31 07:42:37 +0300

    changes the name of the category_id column

--- app/models.py
+++ app/models.py
@@ -1,11 +1,8 @@
-from . import db,login_manager
+from . import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 
 class User(db.Model,UserMixin):
